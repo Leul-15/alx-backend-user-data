ad4d821f142cf5935686af3e9dfb4fcc60a47d7b
Leul <leulassefa255@gmail.com>
2024-05-23 12:21:13 -0700

    Session authentication

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -36,3 +36,10 @@ class Auth:
         """Get
         """
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """Get
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
